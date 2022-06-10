3153d6a7ba13b8adca46055cc2004ed415759e1e
Chebon kibet <43534205+chebon254@users.noreply.github.com>
2022-06-10 15:45:16 +0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += patterns(”, (r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),)
 
