f136b99558590eb8d433c3b3341899a4b92abf95
Chebon kibet <43534205+chebon254@users.noreply.github.com>
2022-06-10 16:37:31 +0300

    Changes to URls Static files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+
 
 # URL Patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
-]
-urlpatterns += patterns(”, (r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),)
-
+    path('', include('store.urls')),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, })
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
